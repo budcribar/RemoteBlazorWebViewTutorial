5f521f14dc9e18d388b5246f1a63c4c055055819
Bud Cribar <budcribar@msn.com>
2020-10-24 09:20:20 -0600

    move tests

---
+++ RemoteBlazorWebViewTutorial.WpfApp/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+
+using BlazorWebView;
+using RemoteBlazorWebViewTutorial.Shared;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RemoteBlazorWebViewTutorial.WpfApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<HttpClient>();
+        }
+
+        /// <summary>
+        /// Configure the app.
+        /// </summary>
+        /// <param name="app">The application builder for apps.</param>
+        public void Configure(ApplicationBuilder app)
+        {
+            app.AddComponent<Shared.App>("app");
+        }
+    }
+}
