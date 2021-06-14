72f8cf485575a43ee0f031ac851c4d817e122c74
Bud Cribar <budcribar@msn.com>
2021-06-14 13:35:30 -0600

    Add restart to winform

---
+++ RemoteBlazorWebViewTutorial.Shared/RunString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteBlazorWebViewTutorial.Shared
+{
+    public class RunString
+    {
+        public Uri? ServerUri { get; set; }
+        public Guid Id { get; set; } //= Guid.Parse("d8d19338-3d66-4942-912b-5b3103efa177");
+        public bool IsRestarting { get; set; } = false;
+
+        public RunString()
+        {
+            IsRestarting = Environment.GetCommandLineArgs().FirstOrDefault(x => x.StartsWith("-r")) != null;
+
+            // -u=https://localhost:443 -i=9BFD9D43-0289-4A80-92D8-6E617729DA12
+            try
+            {
+                var u = Environment.GetCommandLineArgs().FirstOrDefault(x => x.StartsWith("-u"));
+                if (u != null)
+                    ServerUri = new Uri(u.Split("=")[1]);
+            }
+            catch (Exception) { }
+            try
+            {
+                var i = Environment.GetCommandLineArgs().FirstOrDefault(x => x.StartsWith("-i"));
+                if (i != null)
+                    Id = Guid.Parse(i.Split("=")[1]);
+            }
+            catch (Exception) { Id = Guid.NewGuid(); }
+        }
+    }
+}
--- RemoteBlazorWebViewTutorial.WpfApp/MainWindow.xaml.cs
+++ RemoteBlazorWebViewTutorial.WpfApp/MainWindow.xaml.cs
@@ -5,12 +5,14 @@ using RemoteBlazorWebView.Wpf;
 using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http;
+using RemoteBlazorWebViewTutorial.Shared;
 
 namespace RemoteBlazorWebViewTutorial.WpfApp
 {
     public partial class MainWindow : Window
     {
         private bool initialized = false;
+        public RunString Command { get; set; } = new RunString();
 
         public MainWindow()
         {
@@ -19,33 +21,7 @@ namespace RemoteBlazorWebViewTutorial.WpfApp
             serviceCollection.AddScoped<HttpClient>();
             Resources.Add("services", serviceCollection.BuildServiceProvider());
             DataContext = this;
-            ParseRunstring();
             InitializeComponent();
-        }
-
-        public Uri? ServerUri { get; set; }
-        public Guid Id { get; set; } = Guid.Parse("d8d19338-3d66-4942-912b-5b3103efa177");
-        public bool IsRestarting { get; set; } = false;
-   
-        private void ParseRunstring()
-        {
-            IsRestarting = Environment.GetCommandLineArgs().FirstOrDefault(x => x.StartsWith("-r")) != null;
-
-            // -u=https://localhost:443 -i=9BFD9D43-0289-4A80-92D8-6E617729DA12
-            try
-            {
-                var u = Environment.GetCommandLineArgs().FirstOrDefault(x => x.StartsWith("-u"));
-                if (u != null)
-                   ServerUri = new Uri(u.Split("=")[1]);
-            }
-            catch (Exception) { }
-            try
-            { 
-                var i = Environment.GetCommandLineArgs().FirstOrDefault(x => x.StartsWith("-i"));
-                if (i != null)
-                    Id = Guid.Parse(i.Split("=")[1]);
-            }
-            catch (Exception) { Id = Guid.NewGuid(); }
         }
 
         private void Window_ContentRendered(object sender, EventArgs e)
@@ -54,27 +30,19 @@ namespace RemoteBlazorWebViewTutorial.WpfApp
             {
                 this.initialized = true;
 
-                var rbwv = RemoteBlazorWebView;// as IBlazorWebView;
+                IBlazorWebView? rbwv = RemoteBlazorWebView as IBlazorWebView;
                 if (rbwv == null) return;
-                rbwv.Loaded += (x, y) => 
-                    //MessageBox.Show("Loaded");
-                rbwv.Unloaded += (x, y) => Restart();
+               
+                rbwv.Unloaded += (x, y) => { 
+                    Application.Current.Dispatcher.Invoke(() => {
+                        rbwv.Restart();
+                        Close();
+                    });
+                };
             }
         }
 
-        private void Restart()
-        {
-            var psi = new ProcessStartInfo
-            {
-                FileName = Process.GetCurrentProcess().MainModule?.FileName
-            };
-            psi.ArgumentList.Add($"-u={ServerUri}");
-            psi.ArgumentList.Add($"-i={Id}");
-            psi.ArgumentList.Add($"-r=true");
-
-            Process.Start(psi);
-            Application.Current.Dispatcher.Invoke(Close);
-        }
+       
        
     }
 }
