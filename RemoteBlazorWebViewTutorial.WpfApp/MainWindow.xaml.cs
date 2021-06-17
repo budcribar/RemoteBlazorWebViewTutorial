c8b9a35a484107d066880438e5a06c0441ff53d4
Bud Cribar <budcribar@msn.com>
2021-06-17 09:08:41 -0600

    Cleanup naming

--- RemoteBlazorWebViewTutorial.WpfApp/MainWindow.xaml.cs
+++ RemoteBlazorWebViewTutorial.WpfApp/MainWindow.xaml.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Windows;
-using RemoteBlazorWebView.Wpf;
-using System.Diagnostics;
-using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http;
+﻿using Microsoft.Extensions.DependencyInjection;
+using PeakSWC.RemoteableWebView;
 using RemoteBlazorWebViewTutorial.Shared;
-using PeakSwc.RemoteableWebWindows;
+using System;
+using System.Net.Http;
+using System.Windows;
 using System.Windows.Navigation;
 
 namespace RemoteBlazorWebViewTutorial.WpfApp
@@ -17,12 +14,12 @@ namespace RemoteBlazorWebViewTutorial.WpfApp
         public RunString Command { get; set; } = new RunString();
         public Visibility ShowHyperlink { get; set; }
         public Visibility ShowWebView { get; set; }
-      
+
 
         public MainWindow()
         {
             ShowHyperlink = (Command.ServerUri == null || Command.IsRestarting) ? Visibility.Hidden : Visibility.Visible;
-            ShowWebView = Command.ServerUri == null ? Visibility.Visible : Visibility.Hidden;       
+            ShowWebView = Command.ServerUri == null ? Visibility.Visible : Visibility.Hidden;
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddBlazorWebView();
             serviceCollection.AddScoped<HttpClient>();
@@ -44,9 +41,11 @@ namespace RemoteBlazorWebViewTutorial.WpfApp
                     HyperLink.NavigateUri = new Uri($"{rbwv.ServerUri}app/{rbwv.Id}");
                     LinkText.Text = $"{rbwv.ServerUri}app/{rbwv.Id}";
                 }
-               
-                rbwv.Unloaded += (x, y) => { 
-                    Application.Current.Dispatcher.Invoke(() => {
+
+                rbwv.Unloaded += (x, y) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
                         rbwv.Restart();
                         Close();
                     });
