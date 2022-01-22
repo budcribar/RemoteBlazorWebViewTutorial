08aab3b87bc681bdcc8dfa97b12a45359854e9a5
Bud Cribar <budcribar@msn.com>
2022-01-22 05:54:22 -0700

    Photino

---
+++ RemotePhotinoBlazor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using PeakSWC.RemoteWebView;
+using RemoteBlazorWebViewTutorial.Shared;
+
+namespace Photino.Blazor.Sample
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var appBuilder = RemotePhotinoBlazorAppBuilder.CreateDefault(args);
+
+            appBuilder.Services.AddLogging();
+            appBuilder.Services.AddScoped<HttpClient>();
+
+            // register root component and selector
+            appBuilder.RootComponents.Add<App>("#app");
+
+            var app = appBuilder.Build();
+
+            // customize window
+            app.MainWindow.SetTitle("Remote Photino Blazor Sample");
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.OpenAlertWindow("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
+
+        }
+    }
+}
